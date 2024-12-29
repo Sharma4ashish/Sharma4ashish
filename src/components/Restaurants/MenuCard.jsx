a8aecfdc11373d0450190455af3c9705d3c41d47
Ashish Sharma <sharma4shish@gmail.com>
2024-12-29 17:18:10 +0530

    restsurant detail

---
+++ src/components/Restaurants/MenuCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+
+
+function MenuCard() {
+  return (
+    <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <div className='lg:flex items-center justify-between'>
+
+          </div>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+  )
+}
+
+export default MenuCard
--- src/components/Restaurants/RestaurantDetails.jsx
+++ src/components/Restaurants/RestaurantDetails.jsx
@@ -1,118 +1,153 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-import { 
+import {
   Divider,
   FormControl,
   Grid,
-  RadioGroup, 
+  RadioGroup,
   Typography,
   FormControlLabel,
   Radio,
- } from '@mui/material'
+} from "@mui/material";
+
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import MenuCard from "./MenuCard";
 
 
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 
-const DemoCategory = [
-  "pasta",
-  "pizza",
-  "sushi",
-  "burger",
-  "momo"
-]
+
+
+
+const menuItems = [1,1,1,11,1,,]
+
+const demoCategory = ["Pasta", "Pizza", "Sushi", "Burger", "Momo"];
 
 const demoFoodTypes = [
-  {label:"All",value:"all"},
-  {label:"Non-Vegetarian",value:"non_vegetarian"},
-  {label:"Vegetarian",value:"vegetarian"}
-
-]
-
-
-
-
-
-
-
-
-
+  { label: "All", value: "all" },
+  { label: "Non-Vegetarian", value: "non_vegetarian" },
+  { label: "Vegetarian", value: "vegetarian" },
+];
 
 function RestaurantDetails() {
-  const [foodtype, setfoodtype] = useState("all")
+  const [foodtype, setfoodtype] = useState("all");
+  const [foodCategory, setfoodCategory] = useState("all");
 
+  const handleFilter = (e) => {
+    // console.log(e.target.value,e.target.name);
+    setfoodtype(e.target.value);
+  };
 
   return (
-    <div className=' px-5 lg:px-20 ' >
-      <section className=''>
+    <div className=" px-5 lg:px-20 ">
+      <section className="">
         <h3>home/indian/indian-restaurant</h3>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <img className=' w-full h-[40vh] object-cover ' src='https://static.wanderon.in/wp-content/uploads/2024/01/ryu-rooftop-1-min-1600x900-1.webp' />
+            <img
+              className=" w-full h-[40vh] object-cover "
+              src="https://static.wanderon.in/wp-content/uploads/2024/01/ryu-rooftop-1-min-1600x900-1.webp"
+            />
           </Grid>
           <Grid item xs={12} lg={6}>
-            <img className=' w-full h-[40vh] object-cover ' src='https://static.wanderon.in/wp-content/uploads/2024/01/ryu-rooftop-1-min-1600x900-1.webp' />
+            <img
+              className=" w-full h-[40vh] object-cover "
+              src="https://static.wanderon.in/wp-content/uploads/2024/01/ryu-rooftop-1-min-1600x900-1.webp"
+            />
           </Grid>
           <Grid item xs={12} lg={6}>
-            <img className=' w-full h-[40vh] object-cover ' src='https://static.wanderon.in/wp-content/uploads/2024/01/ryu-rooftop-1-min-1600x900-1.webp' />
+            <img
+              className=" w-full h-[40vh] object-cover "
+              src="https://static.wanderon.in/wp-content/uploads/2024/01/ryu-rooftop-1-min-1600x900-1.webp"
+            />
           </Grid>
         </Grid>
-        <div className=' pt-3 pb-3'>
-          <div className=''>
-            <h1 className=' font-semibold text-4xl '>Indian Restroom</h1>
-            <p1 className='text-gray-600 py-1 font-normal' >Lrispy fried chicken is a standard dish in the Cantonese cuisine of southern China and Hong Kong. Lip Sync allows you to synchronize your Text to Speech scripts or uploaded audio to animate a photo or video of your choice. The license has been updated to Apache 2.0, and we've removed the non-commercial restriction</p1>
-            <div className='flex flex-col py-2 gap-2'>
-              <p1 className=' text-gray-600   '>
+        <div className=" pt-3 pb-3">
+          <div className="">
+            <h1 className=" font-semibold text-4xl ">Indian Restroom</h1>
+            <p1 className="text-gray-500 py-1 font-normal">
+              Lrispy fried chicken is a standard dish in the Cantonese cuisine
+              of southern China and Hong Kong. Lip Sync allows you to
+              synchronize your Text to Speech scripts or uploaded audio to
+              animate a photo or video of your choice. The license has been
+              updated to Apache 2.0, and we've removed the non-commercial
+              restriction
+            </p1>
+            <div className="flex flex-col py-2 gap-2">
+              <p1 className=" text-gray-600   ">
                 <LocationOnIcon />
                 <span> Hongs Kichen CP </span>
               </p1>
-              <p1 className=' text-gray-600 '>
-                <CalendarMonthIcon/>
-                <span> Mon 9.00 AM - Sat 11.00 PM</span>              
+              <p1 className=" text-gray-600 ">
+                <CalendarMonthIcon />
+                <span> Mon 9.00 AM - Sat 11.00 PM</span>
               </p1>
             </div>
           </div>
-          <Divider/>
+          <Divider />
 
-          <section className='pt-[2rem] lg:flex relative' >
-            <div className=' space-y-10 lg:w-[20%] filter'>
-              <div className=' box space-y-5 lg:sticky top-28'>
-                <Typography variant='h5' sx={{paddingBottom:"1rem"}}>
+          <section className="pt-[2rem] lg:flex relative">
+            <div className=" space-y-10 lg:w-[20%] filter">
+              <div className=" box space-y-5 lg:sticky top-28">
+                <Typography variant="h5" sx={{ paddingBottom: "1rem" }}>
                   Food Type
                 </Typography>
-                <FormControl className=' py-10 space-y-5' component={"fieldset"}>
-                  <RadioGroup defaultValue="all" name='food_type' value={demoFoodTypes || "all"} >
-                    {demoFoodTypes.map((item)=>(
-                      <FormControlLabel 
+                <FormControl
+                  className=" py-10 space-y-5"
+                  component={"fieldset"}
+                >
+                  <RadioGroup
+                    onChange={handleFilter}
+                    name="food_type"
+                    value={foodtype}
+                  >
+                    {demoFoodTypes.map((item) => (
+                      <FormControlLabel
                         key={item.value}
                         value={item.value}
                         control={<Radio />}
                         label={item.label}
                       />
                     ))}
-                    
-                    
                   </RadioGroup>
-
-
                 </FormControl>
-
-
+              </div>
+              <Divider />
+              <div className=" box space-y-5 lg:sticky top-28">
+                <Typography variant="h5" sx={{ paddingBottom: "1rem" }}>
+                  Food Category
+                </Typography>
+                <FormControl
+                  className=" py-10 space-y-5"
+                  component={"fieldset"}
+                >
+                  <RadioGroup
+                    onChange={handleFilter}
+                    name="food_category"
+                    value={foodCategory}
+                  >
+                    {demoCategory.map((item) => (
+                      <FormControlLabel
+                        key={item}
+                        value={item}
+                        control={<Radio />}
+                        label={item}
+                      />
+                    ))}
+                  </RadioGroup>
+                </FormControl>
               </div>
             </div>
-            <div classname=' space-y-10 lg:w-[80%] filter'>
 
+            <div className=" space-y-10 lg:w-[80%] filter">
+              {menuItems.map((item)=>(<MenuCard>{item}</MenuCard>))}
             </div>
-
-
           </section>
         </div>
       </section>
-
-
     </div>
-  )
+  );
 }
 
-export default RestaurantDetails
+export default RestaurantDetails;
